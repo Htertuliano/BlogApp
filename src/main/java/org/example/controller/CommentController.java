3d83baceb851782748a28f1db5d95de9e6b4e94f
Haqq Tertuliano <35573633+Htertuliano@users.noreply.github.com>
2024-08-02 16:25:03 -0400

    Update CommentController.java

--- src/main/java/org/example/controller/CommentController.java
+++ src/main/java/org/example/controller/CommentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @ResponseBody
 public class CommentController {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private CommentService commentService;
 
